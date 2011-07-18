7158db570f9825d0452459eaae4e4e8c0a194df6
Aaron Mavrinac <mavrinac@gmail.com>
2011-07-18 16:36:21 -0400

    Add camera calibration pose composition module.

---
+++ python/composition.py
@@ -0,0 +1,46 @@
+"""\
+Module for composing multiple relative camera calibrations.
+
+@author: Aaron Mavrinac
+@organization: University of Windsor
+@contact: mavrin1@uwindsor.ca
+@license: GPL-3
+"""
+
+from adolphus.geometry import Pose
+from hypergraph import Graph, Edge
+from hypergraph.path import dijkstra
+
+
+def absolute_poses(poses, reference):
+    """\
+    Provided a set of relative poses, return a set of poses of all objects with
+    respect to the specified reference frame object.
+
+    The relative poses should be supplied as a dict, with tuple keys, where the
+    key indicates the source and destination frame respectively, and the value
+    contains the pose of the source in the coordinate system of the destination
+    as well as the mean error, in the form {(<str>, <str>): (<Pose>, <float>)}.
+    
+    @param poses: The set of relative poses.
+    @type poses: C{dict} of L{Pose}, C{float}
+    @param reference: The final reference frame.
+    @type reference: C{str}
+    @return: C{dict} of L{Pose}, C{float}
+    """
+    cameras = set([key[0] for key in poses])
+    frames = set([key[i] for key in poses for i in range(2)])
+    cg = Graph(vertices=frames)
+    for pair in poses:
+        cg.add_edge(Edge(pair), weight=poses[pair][1])
+    prev = dijkstra(cg, reference)
+    rposes = {}
+    for camera in cameras:
+        pose, error = Pose(), 0.0
+        frame = camera
+        while prev[frame]:
+            pose += poses[(frame, prev[frame])][0]
+            error += poses[(frame, prev[frame])][1]
+            frame = prev[frame]
+        rposes[camera] = (pose, error)
+    return rposes
