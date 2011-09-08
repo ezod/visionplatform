74bd7713c30849edc20a5f27a2d8caa1c4b452ae
Aaron Mavrinac <mavrinac@gmail.com>
2011-09-07 20:50:19 -0400

    Split the actual query portion of the robot position querier into a separate function.

--- python/robotjpos.py
+++ python/robotjpos.py
@@ -18,6 +18,29 @@ import sys
 import serial
 from optparse import OptionParser
 
+
+def query_position(port):
+    """\
+    Query the robot position (given an open serial connection to the robot
+    controller).
+
+    @param port: Open serial connection to the robot controller.
+    @type port: L{serial.Serial}
+    @return 
+    """
+    port.write('1;1;JPOSF.\r')
+    rawpos = ''
+    while True:
+        rawpos += port.read(128)
+        try:
+            if len(rawpos) > 3 and not rawpos.startswith('QoK'):
+                raise ValueError('non-QoK response from robot')
+            splitpos = rawpos.split(';;')[0][3:].split(';')
+            return [float(splitpos[2 * i + 1])  for i in range(6)]
+        except IndexError:
+            continue
+
+
 if __name__ == '__main__':
     parser = OptionParser()
     parser.add_option('-s', '--serialport', dest='serialport', action='store',
@@ -31,22 +54,11 @@ if __name__ == '__main__':
     p = 1
     try:
         while True:
-            port.write('1;1;JPOSF.\r')
-            rawpos = ''
-            while True:
-                rawpos += port.read(128)
-                try:
-                    if len(rawpos) > 3 and not rawpos.startswith('QoK'):
-                        print('Warning: non-QoK at position %i' % p)
-                        p -= 1
-                        break
-                    splitpos = rawpos.split(';;')[0][3:].split(';')
-                    pos = (p,) + tuple([float(splitpos[2 * i + 1]) \
-                        for i in range(6)])
-                    f.write('J%i=(%+.2f,%+.2f,%+.2f,%+.2f,%+.2f,%+.2f)\n' % pos)
-                    break
-                except IndexError:
-                    continue
+            try:
+                pos = (p,) + tuple(query_position(port))
+            except ValueError:
+                continue
+            f.write('J%i=(%+.2f,%+.2f,%+.2f,%+.2f,%+.2f,%+.2f)\n' % pos)
             p += 1
     finally:
         port.close()
