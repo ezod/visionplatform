d182933db4e3b0966bd2d6495ad260c7f1e73edb
Aaron Mavrinac <mavrinac@gmail.com>
2011-10-13 15:35:36 -0400

    Make current and threshold work, and printing values optional.

--- python/selectionsim.py
+++ python/selectionsim.py
@@ -21,8 +21,8 @@ def interpolate_points(points):
     return lambda t: Point([f[i](t) for i in range(3)])
 
 
-def best_view(time, model, relevance, ocular=1, current=None, threshold=0,
-              candidates=None):
+def best_view(model, relevance, ocular=1, current=None, threshold=0,
+              candidates=None, time=None):
     if candidates:
         scores = dict.fromkeys(candidates)
     else:
@@ -31,8 +31,9 @@ def best_view(time, model, relevance, ocular=1, current=None, threshold=0,
         scores[view] = model.performance(relevance, subset=view)
     if current:
         scores[current] += threshold
-    print('%d,' % time + ('%f,' * len(scores))[:-1] % tuple([scores[view] \
-        for view in [frozenset([chr(i)]) for i in range(65,88)]]))
+    if time is not None:
+        print('%d,' % time + ('%f,' * len(scores))[:-1] % tuple([scores[view] \
+            for view in [frozenset([chr(i)]) for i in range(65,88)]]))
     return sorted(scores.keys(), key=scores.__getitem__)[-1]
 
 
@@ -44,6 +45,8 @@ if __name__ == '__main__':
         type='float', default=0.0, help='hysteresis threshold')
     parser.add_option('-z', '--zoom', dest='zoom', default=False,
         action='store_true', help='disable camera view and use visual zoom')
+    parser.add_option('-p', '--printvals', dest='printvals', default=False,
+        action='store_true', help='print coverage values')
     opts, args = parser.parse_args()
     points = []
     for line in open(args[1], 'r'):
@@ -53,9 +56,13 @@ if __name__ == '__main__':
     experiment.execute('loadmodel %s' % args[0])
     experiment.execute('loadconfig %s' % opts.conf)
     experiment.start()
-    print('Time,' + ('%s,' * 23)[:-1] % tuple([chr(i) for i in range(65,88)]))
+    if opts.printvals:
+        print('Time,' + ('%s,' * 23)[:-1] % tuple([chr(i) for i in range(65,88)]))
+    best = experiment.model.views().pop()
     for t in range(100 * (len(points) - 1)):
         experiment.model[args[2]].set_absolute_pose(Pose(T=f(t / 100.0)))
         experiment.model[args[2]].update_visualization()
-        best = best_view(t, experiment.model, experiment.relevance_models[args[3]])
+        best = best_view(experiment.model, experiment.relevance_models[args[3]],
+            current=best, threshold=opts.threshold,
+            time=opts.printvals and t or None)
         experiment.execute('select %s' % set(best).pop())
