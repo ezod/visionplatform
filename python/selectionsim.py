03ab8d104677f4b0b00a7ec59f2e72bc30f23411
Aaron Mavrinac <mavrinac@gmail.com>
2011-10-18 13:08:10 -0400

    Add error modeling for cameras and target.

--- python/selectionsim.py
+++ python/selectionsim.py
@@ -16,33 +16,28 @@ import numpy
 from scipy.interpolate import interp1d
 from optparse import OptionParser
 import os.path
-from random import uniform
 
-from adolphus import Pose, Point, Rotation, Experiment
+import adolphus as A
 
 
 def interpolate_points(points):
     split = [numpy.array([p[i] for p in points]) for i in range(3)]
     tm = [float(i) for i in range(len(points))]
     f = [interp1d(tm, p, kind='cubic') for p in split]
-    return lambda t: Point([f[i](t) for i in range(3)])
+    return lambda t: A.Point([f[i](t) for i in range(3)])
 
 
-def random_pose_error(pose, terror, rerror):
-    T, R = pose.T, pose.R
-    # position error
-    while True:
-        rv = Point((uniform(-1, 1), uniform(-1, 1), uniform(-1, 1)))
-        if rv.magnitude < 1:
-            break
-    T += terror * rv.normal
-    # rotation error
-    while True:
-        rv = Point((uniform(-1, 1), uniform(-1, 1), 0))
-        if rv.magnitude < 1:
-            break
-    R = Rotation.from_axis_angle(rerror, rv.normal) + R
-    return Pose(T=T, R=R)
+def create_error_model(model, terror, rerror):
+    emodel = A.Model(task_params=model._task_params)
+    for camera in set(model.cameras):
+        emodel[camera] = A.Camera(camera, model[camera]._params,
+            pose=model[camera]._pose, mount=model[camera].mount)
+        emodel[camera].set_absolute_pose(\
+            A.random_pose_error(model[camera].pose, terror, rerror))
+    for sceneobj in model.keys():
+        if not sceneobj in model.cameras:
+            emodel[sceneobj] = model[sceneobj]
+    return emodel
 
 
 def best_view(model, relevance, ocular=1, current=None, threshold=0,
@@ -56,7 +51,7 @@ def best_view(model, relevance, ocular=1, current=None, threshold=0,
     if current:
         scores[current] += threshold
     best = sorted(scores.keys(), key=scores.__getitem__)[-1]
-    return best, scores[best]
+    return best, scores[best] - (best == current and threshold or 0)
 
 
 if __name__ == '__main__':
@@ -73,12 +68,16 @@ if __name__ == '__main__':
         action='store_true', help='disable camera view and use visual zoom')
     parser.add_option('-g', '--graph', dest='graph', action='store',
         default=None, help='pickle of vision graph')
+    parser.add_option('-C', '--camera-error', dest='cerror', action='store',
+        nargs=2, type='float', default=None, help='camera calibration error')
+    parser.add_option('-T', '--target-error', dest='terror', action='store',
+        nargs=2, type='float', default=None, help='target pose error')
     opts, args = parser.parse_args()
     points = []
     for line in open(args[1], 'r'):
-        points.append(Point([float(s) for s in line.rstrip().split(',')]))
+        points.append(A.Point([float(s) for s in line.rstrip().split(',')]))
     f = interpolate_points(points)
-    experiment = Experiment(zoom=opts.zoom)
+    experiment = A.Experiment(zoom=opts.zoom)
     experiment.add_display()
     experiment.execute('loadmodel %s' % args[0])
     experiment.execute('loadconfig %s' % opts.conf)
@@ -91,6 +90,15 @@ if __name__ == '__main__':
             pickle.dump(vision_graph, open(opts.graph, 'w'))
         except IndexError:
             vision_graph = None
+    if opts.cerror:
+        emodel = create_error_model(experiment.model, opts.cerror[0],
+            opts.cerror[1])
+    else:
+        emodel = None
+    if opts.terror:
+        etarget = A.RelevanceModel(experiment.relevance_models[args[3]].original)
+    else:
+        etarget = None
     if opts.visualize:
         experiment.start()
     best = None
@@ -103,22 +111,29 @@ if __name__ == '__main__':
         if experiment.exit:
             break
         normal = (f(t / 100.0) - f((t - 1) / 100.0)).normal
-        angle = Point((0, -1, 0)).angle(normal)
-        axis = Point((0, -1, 0)) ** normal
-        R = Rotation.from_axis_angle(angle, axis)
-        experiment.model[args[2]].set_absolute_pose(Pose(T=f(t / 100.0), R=R))
+        angle = A.Point((0, -1, 0)).angle(normal)
+        axis = A.Point((0, -1, 0)) ** normal
+        R = A.Rotation.from_axis_angle(angle, axis)
+        experiment.model[args[2]].set_absolute_pose(\
+            A.Pose(T=f(t / 100.0), R=R))
         if opts.visualize:
             experiment.model[args[2]].update_visualization()
         current = best
-        best, score = best_view(experiment.model, experiment.relevance_models[\
-            args[3]], current=(current and frozenset([current]) or None),
-            threshold=opts.threshold, candidates=((vision_graph and score) \
-            and [frozenset(c) for c in vision_graph.neighbors(current) | \
-            set([current])] or None))
+        if etarget:
+            etarget.set_absolute_pose(A.random_pose_error(experiment.\
+                relevance_models[args[3]].pose, opts.terror[0], opts.terror[1]))
+        best, score = best_view(emodel or experiment.model, etarget or \
+            experiment.relevance_models[args[3]], current=(current and \
+            frozenset([current]) or None), threshold=opts.threshold,
+            candidates=((vision_graph and score) and [frozenset(c) for c in \
+            vision_graph.neighbors(current) | set([current])] or None))
+        if emodel or etarget:
+            score = experiment.model.performance(\
+                experiment.relevance_models[args[3]], subset=best)
         best = set(best).pop()
         current_frames += 1
         if current_frames > opts.jitter:
-            performance += score - (current == best and opts.threshold or 0.0)
+            performance += score
         if current != best:
             if opts.visualize:
                 experiment.execute('select %s' % best)
@@ -129,5 +144,5 @@ if __name__ == '__main__':
                     pass
                 experiment.execute('fov %s' % best)
             current_frames = 0
-    print('Performance (J = %d, T = %g): %f' % (opts.jitter, opts.threshold,
-        (100 * performance / t)))
+    print('Performance (j = %d, t = %g, C = %s, T = %s): %f' % (opts.jitter,
+        opts.threshold, opts.cerror, opts.terror, (100 * performance / t)))
