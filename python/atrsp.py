2eee405f820a727b7d56ebfb50ca4f1762d7d4d6
Aaron Mavrinac <mavrinac@gmail.com>
2012-02-23 16:52:36 -0500

    Fix orientation of camera in camera pose function.

--- python/atrsp.py
+++ python/atrsp.py
@@ -68,10 +68,10 @@ class LensLUT(object):
         
 
 def camera_pose_from_xhdb(x, h, d, beta):
-    y = d * sin(beta)
+    y = -d * sin(beta)
     z = d * cos(beta) + h
     T = Point((x, y, z))
-    R = Rotation.from_axis_angle(pi - beta, Point((1, 0, 0)))
+    R = Rotation.from_axis_angle(pi + beta, Point((1, 0, 0)))
     return Pose(T, R)
 
 
