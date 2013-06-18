87a0ce004a9f8cf0c5f5d4f2c88c2a5ecdd25918
Jose Alarcon <alarconj@uwindsor.ca>
2013-06-18 13:41:05 -0400

    Finished TODO's from last time.

--- python/othermodels.py
+++ python/othermodels.py
@@ -38,7 +38,7 @@ class HorsterLienhartCamera(Camera):
         occlusion is computed in the L{Model} object instead.
 
         Assumptions:
-        
+
             * High walls (infinite planes induced by M{x}-M{y} line segments).
             * Cameras are rotated only about M{z}.
 
@@ -158,7 +158,7 @@ class ScottTask(RangeTask):
 
     The L{ScottTask} adds to the set of task parameters:
 
-        - C{sample_density}: The number of samples along the transport direction 
+        - C{sample_density}: The number of samples along the transport direction
                              in one range image.
     """
     defaults = copy(Task.defaults)
@@ -166,19 +166,19 @@ class ScottTask(RangeTask):
 
 class ScottCamera(RangeCamera):
     """\
-        * W. R. Scott, "Model-Based View Planning," Machine Vision and Applications, 
+        * W. R. Scott, "Model-Based View Planning," Machine Vision and Applications,
           vol. 20, no. 1, pp. 47-69, 2009.
-        * W. R. Scott, G. Roth, and J. F. Rivest, "Pose Error Effects on Range Sensing," 
+        * W. R. Scott, G. Roth, and J. F. Rivest, "Pose Error Effects on Range Sensing,"
           In 15th Int. Conf. on Vision Interface, Calgary, pp. 331-338, 2002.
-        * S. El-Hakim, J. A. Beraldin, "Configuration Design for Sensor Integration," 
+        * S. El-Hakim, J. A. Beraldin, "Configuration Design for Sensor Integration,"
           In Proc. Videometrics IV, SPIE, Philadelphia, vol. 2598, pp. 274-285, 1995.
 
-        The general model is presented in [Scott 2009], however many implementation 
+        The general model is presented in [Scott 2009], however many implementation
         details are drawn from [Scott 2002] and [El-Hakim 1995].
     """
     def unit_step(x):
         """\
-        This function implements the unit step function and is implemented mainly 
+        This function implements the unit step, and is implemented mainly
         for ease of readability.
 
         @param x: The input value.
@@ -188,12 +188,12 @@ class ScottCamera(RangeCamera):
         """
         return 0 if x <= 0 else 1
 
-    def mp(p, theta_xz, theta_yz):
+    def mp(cp, theta_xz, theta_yz):
         """\
         Measurement precision.
 
-        @param p: The point to test.
-        @type p: L{Point}
+        @param cp: The point to test, already in camera coordinates.
+        @type cp: L{Point}
         @param theta_xz: The incidence angle in the x-z plane.
         @type theta_xz: L{Angle}
         @param theta_yz: The incidence angle in the y-z plane.
@@ -201,32 +201,26 @@ class ScottCamera(RangeCamera):
         @return: The measurement precision.
         @rtype: C{bool}
         """
-        # C_z is the noise coefficient obtained from curve fitting on calibrated data.
-        # See El-Hakim 1995.
-        # TODO: Set C_z to an appropriate value, see El-Hakim 1995.
-        C_z = 1.
-        # TODO: Find a way to obtain t_yz and t_xz from the parameter angle_max.
-        # t_yz is the cut-off angle on the y-z plane at which the sensor can not produce 
-        # an output.
-        t_yz = (60. * pi) / 180.
-        # t_xz is the cut-off angle on the x-z plane at which the sensor can not produce 
-        # an output.
-        t_xz = (70. * pi) / 180.
+        # C_z is the z_S parameter, see El-Hakim 1995, Section 5.2.
+        C_z = self.getparam('zS')
 
+        # t_yz is the cut-off angle on the y-z plane at which the sensor can not
+        # produce an output, Scott 2009 Section 2.3.1.
+        t_yz = (60. * pi) / 180.
+        t_xz = (70. * pi) / 180.
         try:
-            sigma_z = (C_z * p.z**2) / \
+            sigma_z = (C_z * cp.z**2) / \
                 (cos(theta_yz) * (1 - self.unit_step(abs(theta_yz) - t_yz)) * \
                 (1 - self.unit_step(abs(theta_xz) - t_xz)))
         except ZeroDivisionError:
             sigma_z = 0
-
         return sigma_z
 
     def sd(cp, tp, theta_xz, theta_yz):
         """\
         Sampling density.
 
-        @param p: The point to test.
+        @param p: The point to test, already in camera coordinates.
         @type p: L{Point}
         @param tp: Task parameters.
         @type tp: C{dict}
@@ -241,23 +235,21 @@ class ScottCamera(RangeCamera):
         phi_x = self.fov['ah']
 
         # f_d is a standoff distance adjustment, f_d = 1 + d, d << 1.
-        # See Scott 2002.
+        # See Scott 2009, Section 4.4.3.
         f_d = 1.02
 
         # R_o is the optimum sensor scanning range.
-        # See Scott 2002.
+        # See Scott 2009, Section 4.4.3.
         R_o = max(self.zres(task_params['res_max'][1]), \
                   self.zc(task_params['blur_max'][1] * \
                   min(self._params['s']))[0])
 
-        # phi_xz is the instantaneous laser scan angle in the x-z plane.
-        # NOTE: According to Scott 2002 setting phi_xz to zero and z_s = f_d * R_o 
-        # allows for optimal viewpoints. Additionally it is not clear at all from the 
-        # literature how to obtain phi_xz if it were not set to zero.
-        # See Scott 2002.
+        # phi_xz is the angle between the z axis in the wcs and the line between the
+        # scene point and the laser. By construction of the viewpoint space, this
+        # angle can not be anything other than zero.
         phi_xz = 0
 
-        # R_xz is the slant range. See Scott 2002
+        # R_xz is the slant range. See Scott 2009, Section 2.3.1.
         R_xz = cp.z / cos(phi_xz)
 
         N_x = self._params['dim'][0]
@@ -289,17 +281,13 @@ class ScottCamera(RangeCamera):
         except AttributeError:
             raise TypeError('point must be directional for range coverage')
 
-        # theta_yz is the incidence angle in the y-z plane of the laser with respect to 
+        # theta_yz is the incidence angle in the y-z plane of the laser with respect to
         # the normal of the point's surface (in the global reference frame).
-        # See Scott 2009
+        # See Scott 2009, Section 4.4.3.
         temp_p = Point(0., point.direction_unit().y, point.direction_unit().z)
-        # This makes the assumption that the laser plane is orthogonal to the transport 
+        # This makes the assumption that the laser plane is orthogonal to the transport
         # direction.
         theta_yz = Point(0., 0., 1.).angle(temp_p)
-
-        # theta_xz is the incidence angle in the x-z plane of the laser with respect to 
-        # the normal of the point's surface (in the global reference frame).
-        # See Scott 2009
         temp_p = Point(point.direction_unit().x, 0., point.direction_unit().z)
         theta_xy = Point(0., 0., 1.).angle(temp_p)
 
